1393888daa3e788b88bda01143fafa7d3c3ed14f
Pasindu Shavinda <pasindu.shv@gmail.com>
2022-09-15 16:57:16 +0530

    Add Garden Designer Requirements Backend Implemented

--- routes/shvClientGDRoute.js
+++ routes/shvClientGDRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const AdminGD = require('../models/shvAdminGDModel.js');
+const ClientGD = require('../models/shvClientGDModel.js');
 //const multer = require('multer');
 //const fs = require('fs');
-//const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
+const session = require("express-session");
 //const path = require('path');
 
 
@@ -11,7 +13,7 @@ const AdminGD = require('../models/shvAdminGDModel.js');
 router.get("/allClientGD", (req, res) => {
   AdminGD.find().exec((err, adminGds) => {
     if (err) {
-      res.json({ message: err.message });
+      res.json({ message: err.message });b
     } else {
       res.render("shvClientViewAllGardenDesigners", {
         title: "All Client Garden Designers",
@@ -19,6 +21,76 @@ router.get("/allClientGD", (req, res) => {
       });
     }
   });
+});
+
+
+// Add Garden Designer Requirements Route
+router.post("/addClientGDReq/:id", [
+// Validations
+  check('ClientName')
+    .matches(/^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/)
+    .withMessage('Invalid Name.. Please enter correct name !!'),
+  
+  check('Email')
+    .matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    .withMessage('Email is not valid.. Please enter correct email !! !'),
+
+  check('Phone')
+    .isLength({ min: 10, max: 10 })
+    .withMessage('Mobile number is not valid.. Please enter correct mobile number !! !'),
+  
+  check('Budget')
+    .matches(/^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/)
+    .withMessage('Budget is not valid.. Please enter correct budget !! !'),
+
+],(req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    const alert = errors.array()
+    let id = req.params.id;
+    AdminGD.findById(id, (err, adminGd) => {
+      if (err) {
+        res.json({ message: err.message });
+      } else {
+        res.render("shvClientAddRequirements.ejs", {
+          title: "Client Add Requirements",
+          adminGd: adminGd,
+          alert
+        });
+      }
+    });
+
+  } else {
+      const GDReq = new ClientGD({
+        GDName : req.body.GDName,
+        ClientName : req.body.ClientName,
+        Email : req.body.Email,
+        Phone : req.body.Phone,
+        GardenArea : req.body.GardenArea,
+        Budget : req.body.Budget,
+        Address : req.body.Address,
+        Grass : req.body.Grass,
+        Pool : req.body.Pool,
+        Interlock : req.body.Interlock,
+        Play : req.body.Play,
+        Flower : req.body.Flower,
+        Pond : req.body.Pond,
+        SpecialReq : req.body.SpecialReq
+      });  
+
+      GDReq.save((err)=>{
+        if(err){
+          res.json({message:err.message, type:'danger'});
+        } else {
+          req.session.message = {
+            type:'success',
+            message:'Requirements Successfully Added'
+          }
+          res.redirect();
+        }
+      })
+
+  }
 });
 
 module.exports = router;
